e7b5d8f905878886787c5b578a230fb34cffe17e
Muskaan Singh <muskaansingh235@gmail.com>
2022-07-04 12:00:22 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='text-center text-3xl text-indigo-600 font-bold pt-40'>Hello world!!!!</div>
+    </div>
+  );
+}
+
+export default App;
