7a61e86fa49eaef0a221c8ee15b6f2cf3042b70b
Muskaan Singh <muskaansingh235@gmail.com>
2022-07-05 10:17:22 +0530

    Fixed react-typed package issue

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Typed from 'react-typed';
+import ReactTypingEffect from 'react-typing-effect';
 
 const Content = () => {
     return (
@@ -10,12 +10,13 @@ const Content = () => {
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow With Data.</h1>
                 <div className='flex justify-center items-center'>
                     <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for </p>
-                    <Typed
-                        className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-                        strings={['BTB', 'BTC', 'SASS']}
+                    <ReactTypingEffect
+                        text={['BTB', 'BTC', 'SASS']}
                         typeSpeed={120}
                         backSpeed={140}
-                        loop />
+                        loop
+                        className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+                    />
                 </div>
                 <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB,
                     BTC and SASS platforms.
@@ -28,4 +29,4 @@ const Content = () => {
     )
 }
 
-export default Content
+export default Content;
